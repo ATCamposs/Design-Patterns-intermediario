a78f10c5b3ef99e3dd569efa4569c4830d7b87d4
ATCamposs <andre.tatibano@gmail.com>
2020-10-12 20:20:31 -0300

    Classe concreta e exemplo de uso com PDO

--- example.php
+++ example.php
@@ -2,14 +2,23 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$builder = new Design\Db\Builder\MySqlBuilder();
+/*$builder = new Design\Db\Builder\MySqlBuilder();
 
-/*$director = new Design\Db\Builder\UsersDirector($builder);
+$director = new Design\Db\Builder\UsersDirector($builder);
 
 $sql = $director->getSqlAll();
-*/
+
 $banks = new App\Model\Banks($builder);
 
 $sql = $banks->getSqlAll();
 
-var_dump($sql);
+var_dump($sql);*/
+$pdo = new \PDO('mysql:host=localhost; dbname=designtestes', 'root', '1234');
+
+$db = new Design\Db\Db($pdo);
+$db->setDirector($director);
+
+$data = $db->getAll()
+    ->execute();
+
+var_dump($data->fetchAll());
