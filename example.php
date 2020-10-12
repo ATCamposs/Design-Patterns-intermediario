ac51864eb053c7fb34cd37f9821ac75ff741465c
ATCamposs <andre.tatibano@gmail.com>
2020-10-12 20:30:48 -0300

    Exemplo de uso com singleton.

--- example.php
+++ example.php
@@ -12,10 +12,25 @@ $banks = new App\Model\Banks($builder);
 
 $sql = $banks->getSqlAll();
 
-var_dump($sql);*/
+var_dump($sql);
 $pdo = new \PDO('mysql:host=localhost; dbname=designtestes', 'root', '1234');
 
 $db = new Design\Db\Db($pdo);
+$db->setDirector($director);
+
+$data = $db->getAll()
+    ->execute();
+
+var_dump($data->fetchAll());
+*/
+$config = [
+    'dsn' => 'mysql:host=localhost; dbname=designtestes',
+    'user' => 'root',
+    'passwd' => '1234'
+];
+
+Design\Db\DbSingleton::configure($config);
+$db = Design\Db\DbSingleton::getInstance();
 $db->setDirector($director);
 
 $data = $db->getAll()
