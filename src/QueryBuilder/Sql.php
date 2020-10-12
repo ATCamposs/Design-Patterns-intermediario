411895b21bc2c75822cb71785dbed9a1ec8b5581
ATCamposs <andre.tatibano@gmail.com>
2020-10-12 19:37:11 -0300

    Implementação exemplo strategy

--- src/QueryBuilder/Sql.php
+++ src/QueryBuilder/Sql.php
@@ -6,11 +6,19 @@ class Sql implements Strategy
 {
     public function table(string $table): Strategy
     {
+        $this->table = $table;
+        return $this;
     }
     public function select($collumns = '*'): Strategy
     {
+        if (is_array($collumns)) {
+            $collumns = implode(', ', $collumns);
+        }
+        $this->sql = sprintf('SELECT %s FROM %s;', $collumns, $this->table);
+        return $this;
     }
-    public function getQuery(): Strategy
+    public function getQuery(): string
     {
+        return $this->sql;
     }
 }
--- src/QueryBuilder/Strategy.php
+++ src/QueryBuilder/Strategy.php
@@ -6,5 +6,5 @@ interface Strategy
 {
     public function table(string $table): Strategy;
     public function select($collumns = '*'): Strategy;
-    public function getQuery(): Strategy;
+    public function getQuery(): string;
 }
